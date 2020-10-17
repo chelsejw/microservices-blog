8a2b37fbb195633372b76462efba6473d5d5f608
chelsejw <chelsejw@gmail.com>
2020-10-17 18:12:17 +0800

    add basic event bus and query service logic

--- comments/index.js
+++ comments/index.js
@@ -2,16 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios')
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const commentsByPostId = {};
-
+const port = process.env.PORT || 4001
 app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
+
+app.post('/events', (req, res) => {
+  console.log(`Received event`, req.body.type)
+  console.log(`Event body`, req.body)
+  res.send(req.body)
+})
 
 app.post('/posts/:id/comments', (req, res) => {
   const commentId = randomBytes(4).toString('hex');
@@ -23,9 +30,17 @@ app.post('/posts/:id/comments', (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
+
+  axios.post('http://localhost:4005/events', {
+    type: "CommentCreated", 
+    data: {
+      id: commentId, content, postId: req.params.id
+    }
+  })
+  
   res.status(201).send(comments);
 });
 
-app.listen(4001, () => {
-  console.log('Listening on 4001');
+app.listen(port, () => {
+  console.log('Listening on port ' + port);
 });
--- posts/index.js
+++ posts/index.js
@@ -6,14 +6,22 @@ const cors = require('cors');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
+const port = process.env.PORT || 4000
+const axios = require('axios');
 const posts = {};
+
+app.post('/events', (req, res) => {
+  console.log(`Received event`, req.body.type)
+  console.log(`Event body`, req.body)
+  res.send(req.body)
+})
+
 
 app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
@@ -22,9 +30,14 @@ app.post('/posts', (req, res) => {
     title
   };
 
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: {id, title}
+  });
+
   res.status(201).send(posts[id]);
 });
 
-app.listen(4000, () => {
-  console.log('Listening on 4000');
+app.listen(port, () => {
+  console.log('Listening on port ' + port);
 });
